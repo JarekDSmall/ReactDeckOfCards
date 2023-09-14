c2094127e459937fd761642fac88d2de5c50ae40
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-14 01:54:17 -0400

    Card Component for Displaying Individual Cards
    Created the Card.js component.
    Set up the component to receive a card prop and display the card image

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card({ card }) {
+  return (
+    <div>
+      <img src={card.image} alt={card.value + ' of ' + card.suit} />
+    </div>
+  );
+}
+
+export default Card;
